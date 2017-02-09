9955ed6e8d44323583c8f801fd322cd12a975eef
Cromalisa <cromalisa96@gmail.com>
2017-02-09 20:13:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,6 @@ console.log('Loaded!');
 var element = document.getelememtById("main-text");
 element.innerHTMl= 'New value';
 var img=document.getElementById('madi');
-img.onclick = function()
-{
+img.onclick = function(){
   img.style.marginLeft='100px'  
 };
